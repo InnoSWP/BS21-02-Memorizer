b611be8ac873ddd34797776e1ffc05f595400631
SaidKamalov <arturrulit5@gmail.com>
2022-06-16 13:22:55 +0300

    add modified player-screen, updated server_provider

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -9,72 +9,117 @@ class PlayerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final serverProvider = Provider.of<ServerProvider>(context);
+    // TODO: implement build
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            color: kMainButtonColor,
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
+          centerTitle: true,
+          title: Text("Memorizer", style: TextStyle(color: Color(0xff6750a4))),
+          backgroundColor: Colors.transparent,
           elevation: 0,
+          leading: IconButton(
+              // ignore: avoid_print
+              onPressed: () => print('Go back'),
+              icon: Icon(Icons.arrow_back, color: Color(0xff6750a4))),
         ),
         body: Column(children: [
           Container(
-            alignment: Alignment.center,
-            // ignore: todo
-            //TODO: Connect with the screen (precentages)
-            width: 370,
-            height: 350,
-            decoration: BoxDecoration(
-              color: const Color(0xFFF6F2FA),
-              border: Border.all(color: const Color(0xFFF6F2FA)),
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-            ),
-            child: Text(
-              serverProvider.results.toString(),
-              style: const TextStyle(
-                fontSize: 24,
+              alignment: Alignment.center,
+              // ignore: todo
+              //TODO: Connect with the screen (precentages)
+              width: MediaQuery.of(context).size.width * 0.95,
+              height: MediaQuery.of(context).size.height * 0.63,
+              decoration: BoxDecoration(
+                color: const Color(0xFFF6F2FA),
+                border: Border.all(color: const Color(0xFFF6F2FA)),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
               ),
-            ),
-          ),
+              child: Column(children: [
+                Container(
+                    margin: const EdgeInsets.only(top: 150),
+                    child: Column(children: [
+                      const Text(
+                        "Oh, I hope some day I'll make it out of here. Even if it takes all night or a hundred years",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 70),
+                          child: const Text(
+                            "Need a place to hide, but I can't find one near. Wanna feel alive, outside I can't fight my fear",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ))
+                    ]))
+              ])),
           Padding(
-              padding: const EdgeInsets.only(top: 30),
+              padding: const EdgeInsets.only(top: 15),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   IconButton(
-                    // ignore: avoid_print
+                      // ignore: avoid_print
                       onPressed: (() => print('Left button pressed')),
-                      icon: Image.asset('assets/left.png')),
+                      icon: Icon(Icons.keyboard_double_arrow_left),
+                      color: Color(0xff4f378b),
+                      iconSize: 50),
                   IconButton(
-                    // ignore: avoid_print
+                      // ignore: avoid_print
                       onPressed: (() => print('Pause button pressed')),
-                      icon: Image.asset('assets/pause.png')),
+                      icon: Icon(Icons.play_arrow),
+                      color: Color(0xff4f378b),
+                      iconSize: 50),
                   IconButton(
-                    // ignore: avoid_print
+                      // ignore: avoid_print
                       onPressed: (() => print('Right button pressed')),
-                      icon: Image.asset('assets/right.png'))
+                      icon: Icon(Icons.keyboard_double_arrow_right),
+                      color: Color(0xff4f378b),
+                      iconSize: 50)
                 ],
               )),
           Padding(
-              padding: const EdgeInsets.only(bottom: 5, top: 5),
-              child: IconButton(
-                // ignore: avoid_print
-                onPressed: () => print('Repeat'),
-                icon: Image.asset('assets/repeat.png'),
-                alignment: Alignment.center,
-              )),
-          IconButton(
-            iconSize: 70,
-            // ignore: avoid_print
-            onPressed: () => print('Voice comands'),
-            icon: Image.asset('assets/mic.png'),
-            alignment: Alignment.center,
-          ),
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Ink(
+                        decoration: const ShapeDecoration(
+                          color: Color(0xffeaddff),
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                            onPressed: () => print("repeat"),
+                            icon: Icon(Icons.repeat),
+                            color: Color(0xff6750a4),
+                            iconSize: 30)),
+                    Ink(
+                        decoration: const ShapeDecoration(
+                          color: Color(0xff6750a4),
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                            onPressed: () => print("Voice commands"),
+                            icon: Icon(Icons.mic_none),
+                            color: Color(0xffeaddff),
+                            iconSize: 40)),
+                    Padding(
+                        padding: const EdgeInsets.only(left: 0),
+                        child: Ink(
+                          decoration: const ShapeDecoration(
+                            color: Color(0xffeaddff),
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                              onPressed: () => print("Settings"),
+                              icon: Icon(Icons.settings),
+                              color: Color(0xff4f378b),
+                              iconSize: 30),
+                        ))
+                  ]))
         ]));
   }
 }
--- lib/server/server_provider.dart
+++ lib/server/server_provider.dart
@@ -7,8 +7,9 @@ class ServerProvider extends ChangeNotifier {
   ServerProvider();
 
   void postText({required String text}) async {
-    Uri url = Uri.parse("https://aqueous-anchorage-93443.herokuapp.com/sentences");
+    Uri url = Uri.parse("https://6aa44587-74aa-4c56-bec3-edaf43b12c52.mock.pstmn.io/sentences");
     List<String> sentences = [];
+    text = "Don’t think of it as anything more than that. Our natural growth isn’t only physical. As our bodies mature our minds and souls do too. It may be that he loves you now, and that his intentions are honorable, but remember who he is. He’s not his own man: he’s subject to his birth. He can’t just do as he likes as the common people can. In time the safety and health of Denmark will depend on his decisions. When he chooses a wife it must be after he has heard and considered the opinions of those institutions that he is the head of. So if he tells you he loves you you should understand that he loves you in as much as a man in his position can, which is no more than the people of Denmark will allow. Decide whether you can cope with it if you suffer disappointment by taking too much notice of his serenades and falling in love with him or surrendering your virginity to him.";
     var response = await http.post(url,
         headers: {
           "Access-Control_Allow_Origin": "*",
