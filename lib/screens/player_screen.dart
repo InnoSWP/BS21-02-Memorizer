f9587ae5eda2903a5e404daadca6580d0f0be2eb
SaidKamalov <arturrulit5@gmail.com>
2022-07-03 19:56:15 +0300

    Add voice commands

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -22,6 +22,7 @@ class _PlayerScreenState extends State<PlayerScreen> {
   // final itemController = ItemScrollController();
   int _currentLine = 0;
   Icon iconMain = const Icon(Icons.play_arrow);
+  Icon iconMicro = const Icon(Icons.mic_none);
   FlutterTts flutterTts = FlutterTts();
   bool onPause = true;
   bool _playbackOn = false;
@@ -58,9 +59,7 @@ class _PlayerScreenState extends State<PlayerScreen> {
 
   void initPicovoice() async {
     String contextAsset = "assets\\Memorizer_en_android_v2_1_0.rhn";
-    //String contextPath = await _extractAsset(contextAsset);
-    String keywordAsset = "assets\\hey-Memorizer_en_android_v2_1_0.ppn";
-    //String keywordPath = await _extractAsset(keywordAsset);
+    String keywordAsset = "assets\\Hey-Memo_en_android_v2_1_0.ppn";
     String accessKey =
         "4CnkZqb8+dM7hDpoc4VVxgXnkDcgnYKvPuu0/OIfQLn3ogrpVwFycQ==";
 
@@ -93,7 +92,6 @@ class _PlayerScreenState extends State<PlayerScreen> {
 
   void wakeWordCallback() {
     print("wake word detected!");
-    listeningForCommand = true;
   }
 
   inferenceCallback(RhinoInference inference) {
@@ -104,13 +102,20 @@ class _PlayerScreenState extends State<PlayerScreen> {
       print(command);
       switch (command) {
         case "play":
-          stopText();
-          print(lines);
-          _playText(lines[_currentLine], settings);
+          playOrpause();
           break;
         case "pause":
-          stopText();
+          playOrpause();
           break;
+        case "prev":
+          playPrev();
+          break;
+        case "next":
+          playNext();
+          break;
+        case "repeat":
+          stopText();
+          speakText(lines[_currentLine], settings);
       }
     }
     //_listeningForCommand = false;
@@ -180,14 +185,18 @@ class _PlayerScreenState extends State<PlayerScreen> {
                     listeningForCommand = !listeningForCommand;
                     if (listeningForCommand) {
                       initPicovoice();
+                      setState(() {
+                        iconMicro = Icon(Icons.mic_off);
+                      });
                       print("Voice commands");
                     } else {
+                      setState(() {
+                        iconMicro = Icon(Icons.mic_none);
+                      });
                       picovoiceManager!.stop();
                     }
-
-                    // print(await flutterTts.getVoices);
                   },
-                  icon: Icon(Icons.mic_none),
+                  icon: iconMicro,
                   color: Color(0xffeaddff),
                   iconSize: 40)),
           Padding(
@@ -240,131 +249,20 @@ class _PlayerScreenState extends State<PlayerScreen> {
                   padding: EdgeInsets.only(right: 0),
                   child: IconButton(
                       // ignore: avoid_print
-                      onPressed: () async {
-                        if (_currentLine >= 1) {
-                          if (!onPause) {
-                            stopText();
-                            speakText(lines[_currentLine - 1], settings); // -1
-                          }
-                          setState(() {
-                            _currentLine--;
-                            pageController.previousPage(
-                                duration: Duration(milliseconds: 800),
-                                curve: Curves.easeIn);
-                          });
-                        }
-                      },
+                      onPressed: playPrev,
                       icon: Icon(Icons.keyboard_arrow_left),
                       color: Color(0xff4f378b),
                       iconSize: 50),
                 ),
                 IconButton(
                     // ignore: avoid_print
-                    onPressed: (() async {
-                      onPause = !onPause;
-                      if (iconMain.icon == Icons.play_arrow) {
-                        setState(() {
-                          iconMain = Icon(Icons.pause);
-                        });
-                        String text = lines[_currentLine];
-                        int reps = settings.repetitions;
-                        var vol = settings.volume;
-                        var speed = settings.speed;
-                        print("vol" + vol.toString());
-                        print("speed" + speed.toString());
-                        await flutterTts.setVoice(
-                            {"name": "en-us-x-tpf-local", "locale": "en-US"});
-                        //await flutterTts.setSpeechRate(speed - 0.5);
-                        //await flutterTts.setVolume(vol);
-                        await flutterTts.setQueueMode(1);
-                        print(reps);
-                        for (int i = 0; i < reps; i++) {
-                          await flutterTts.speak(text);
-                        }
-                        flutterTts.setCompletionHandler(() {
-                          setState(() {
-                            _playbackOn = true;
-                            _currentLine++;
-                          });
-                          pageController.nextPage(
-                              duration: Duration(milliseconds: 800),
-                              curve: Curves.easeIn);
-                          // print('are we jere' + _currentLine.toString());
-                        });
-                        // print('are we here>', currentLin);
-                        // _currentLine++;
-                        // pageController.nextPage(
-                        //     duration: Duration(milliseconds: 800),
-                        //     curve: Curves.easeIn);
-                        // setState(() {
-                        //   iconMain = Icon(Icons.pause);
-                        // });
-                        // // stopText();
-                        // // speakText(lines[_currentLine], settings);
-                        // String text = lines[_currentLine];
-                        // int reps = settings.repetitions;
-                        // var vol = settings.volume;
-                        // var speed = settings.speed;
-                        // print("vol" + vol.toString());
-                        // print("speed" + speed.toString());
-                        // await flutterTts.setVoice(
-                        //     {"name": "en-us-x-tpf-local", "locale": "en-US"});
-                        // //await flutterTts.setSpeechRate(speed - 0.5);
-                        // //await flutterTts.setVolume(vol);
-                        // await flutterTts.setQueueMode(1);
-                        // print(reps);
-                        // for (int i = 0; i < reps; i++) {
-                        //   await flutterTts.speak(text);
-                        // }
-                        // flutterTts.setCompletionHandler(() {
-                        //   print('done!');
-                        //   if (_currentLine < lines.length - 1) {
-                        //     setState(() {
-                        //       print(_currentLine);
-                        //       _currentLine++;
-                        //       _lineComplete = true;
-                        //       print(_currentLine);
-                        //        pageController.nextPage(
-                        //         duration: Duration(milliseconds: 800),
-                        //         curve: Curves.easeIn);
-                        //     });
-                        //   }
-                        // });
-                        // if (_lineComplete) {
-                        //   setState(() {
-                        //     _lineComplete = false;
-                        //     print('wtf $_lineComplete');
-
-                        //   });
-                        // }
-                      } else {
-                        setState(() {
-                          _playbackOn = false;
-                          iconMain = Icon(Icons.play_arrow);
-                        });
-                        stopText();
-                      }
-                    }),
+                    onPressed: playOrpause,
                     icon: iconMain,
                     color: Color(0xff4f378b),
                     iconSize: 50),
                 IconButton(
                     // ignore: avoid_print
-                    onPressed: () {
-                      if (_currentLine < lines.length - 1) {
-                        if (!onPause) {
-                          stopText();
-                          speakText(
-                              lines[_currentLine + 1], settings); // speakText
-                        }
-                        setState(() {
-                          _currentLine++;
-                          pageController.nextPage(
-                              duration: Duration(milliseconds: 800),
-                              curve: Curves.easeIn);
-                        });
-                      }
-                    },
+                    onPressed: playNext,
                     icon: Icon(Icons.keyboard_arrow_right),
                     color: Color(0xff4f378b),
                     iconSize: 50)
@@ -374,6 +272,70 @@ class _PlayerScreenState extends State<PlayerScreen> {
         ],
       ),
     );
+  }
+
+  void playNext() {
+    if (_currentLine < lines.length - 1) {
+      if (!onPause) {
+        stopText();
+        speakText(lines[_currentLine + 1], settings); // speakText
+      }
+      setState(() {
+        _currentLine++;
+        pageController.nextPage(
+            duration: Duration(milliseconds: 800), curve: Curves.easeIn);
+      });
+    }
+  }
+
+  void playOrpause() async {
+    onPause = !onPause;
+    if (iconMain.icon == Icons.play_arrow) {
+      setState(() {
+        iconMain = Icon(Icons.pause);
+      });
+      String text = lines[_currentLine];
+      int reps = settings.repetitions;
+      var vol = settings.volume;
+      var speed = settings.speed;
+      print("vol" + vol.toString());
+      print("speed" + speed.toString());
+      await flutterTts
+          .setVoice({"name": "en-us-x-tpf-local", "locale": "en-US"});
+      await flutterTts.setQueueMode(1);
+      print(reps);
+      for (int i = 0; i < reps; i++) {
+        await flutterTts.speak(text);
+      }
+      flutterTts.setCompletionHandler(() {
+        setState(() {
+          _playbackOn = true;
+          _currentLine++;
+        });
+        pageController.nextPage(
+            duration: Duration(milliseconds: 800), curve: Curves.easeIn);
+      });
+    } else {
+      setState(() {
+        _playbackOn = false;
+        iconMain = Icon(Icons.play_arrow);
+      });
+      stopText();
+    }
+  }
+
+  void playPrev() async {
+    if (_currentLine >= 1) {
+      if (!onPause) {
+        stopText();
+        speakText(lines[_currentLine - 1], settings); // -1
+      }
+      setState(() {
+        _currentLine--;
+        pageController.previousPage(
+            duration: Duration(milliseconds: 800), curve: Curves.easeIn);
+      });
+    }
   }
 
   Widget listElementToScreen(var list, var sizeOfScreen, var index) {
@@ -388,22 +350,18 @@ class _PlayerScreenState extends State<PlayerScreen> {
     );
   }
 
-  Future speakText(var text, Settings setts) async {
+  void speakText(var text, Settings setts) async {
     int reps = setts.repetitions;
     var vol = setts.volume;
     var speed = setts.speed;
     print("vol" + vol.toString());
     print("speed" + speed.toString());
     await flutterTts.setVoice({"name": "en-us-x-tpf-local", "locale": "en-US"});
-    //await flutterTts.setSpeechRate(speed - 0.5);
-    //await flutterTts.setVolume(vol);
     await flutterTts.setQueueMode(1);
     print(reps);
     for (int i = 0; i < reps; i++) {
       await flutterTts.speak(text);
     }
-    //await flutterTts.speak(text);
-    //await flutterTts.speak(text);
   }
 
   Future stopText() async {
